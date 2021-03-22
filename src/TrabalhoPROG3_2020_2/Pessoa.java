c24ae1d17c61791486d71808ec09e68b06251806
AHalic <sophie.gama@aluno.ufes.br>
2021-03-22 20:54:29 -0300

    Adiciona propriedades a Pessoa

--- src/TrabalhoPROG3_2020_2/Pessoa.java
+++ src/TrabalhoPROG3_2020_2/Pessoa.java
@@ -1,5 +1,30 @@
 package TrabalhoPROG3_2020_2;
 
-public class Pessoa {
+import java.time.LocalDate;
 
+public class Pessoa {
+	private String nome;
+	private String genero;
+	private LocalDate nascimento;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public LocalDate getNascimento() {
+		return nascimento;
+	}
+	public void setNascimento(LocalDate nascimento) {
+		this.nascimento = nascimento;
+	}
+	
+	
 }
