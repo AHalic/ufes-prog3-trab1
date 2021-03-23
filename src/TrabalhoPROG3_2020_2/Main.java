d1350c1b6d53984a398ce2b6bf8665f9be197803
bea <beamaia.work@gmail.com>
2021-03-23 00:53:04 -0300

    estuda diferentes formas de leitura

--- src/TrabalhoPROG3_2020_2/Main.java
+++ src/TrabalhoPROG3_2020_2/Main.java
@@ -1,7 +1,37 @@
 package TrabalhoPROG3_2020_2;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.nio.charset.StandardCharsets;
 
 public class Main {
 	public static void main(String[] args) {
-		
+
+		try {
+		      File myObj = new File("filename.txt");
+		      Scanner myReader = new Scanner(myObj);
+		      while (myReader.hasNextLine()) {
+		        String data = myReader.nextLine();
+		        System.out.println(data);
+		      }
+		      myReader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		  
+		  try (FileInputStream fis = new FileInputStream(file);
+		       InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+		       BufferedReader reader = new BufferedReader(isr)
+		  ) {
+
+		      String str;
+		      while ((str = reader.readLine()) != null) {
+		          System.out.println(str);
+		      }
+
+		  } catch (IOException e) {
+		      e.printStackTrace();
+		  }
 	}
 }
