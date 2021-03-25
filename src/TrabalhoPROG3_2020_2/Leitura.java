195030f02266ec356fc88aba45bf2b40ed762686
bea <beamaia.work@gmail.com>
2021-03-25 20:37:38 -0300

    add leitura partido

---
+++ src/TrabalhoPROG3_2020_2/Leitura.java
@@ -0,0 +1,37 @@
+package TrabalhoPROG3_2020_2;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Leitura {
+    String caminhoLeitura;
+
+    public Partido criaPartido (Scanner linhaScannerP) {
+        int numPartido = Integer.parseInt(linhaScannerP.next());
+        int votosLegenda = Integer.parseInt(linhaScannerP.next());
+        String nomePartido = linhaScannerP.next();
+        String sigla = linhaScannerP.next();
+        Partido partido = new Partido(nomePartido, sigla, votosLegenda, numPartido);
+
+        return partido;
+    }
+
+    public Set<Partido> lerPartido (Scanner sP) {
+        Set<Partido> partidos = new HashSet<Partido>();
+
+        while (sP.hasNextLine()) {
+            String linha = sP.nextLine();
+
+            Scanner linhaScannerP = new Scanner(linha);
+            linhaScannerP.useDelimiter(",");
+
+            Partido partido = criaPartido(linhaScannerP);
+            partidos.add(partido);
+            
+            linhaScannerP.close();
+        }
+
+        return partidos;
+    }
+}
