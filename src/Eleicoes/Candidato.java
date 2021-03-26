2e906686ad3c14060ba23a579908f180ce4d9774
bea <beamaia.work@gmail.com>
2021-03-25 21:19:51 -0300

    coloca espaço

--- src/Eleicoes/Candidato.java
+++ src/Eleicoes/Candidato.java
@@ -24,30 +24,39 @@ public class Candidato extends Pessoa{
 	public String getSituacao() {
 		return situacao;
 	}
+
 	public void setSituacao(String situacao) {
 		this.situacao = situacao;
 	}
+
 	public String getNomeUrna() {
 		return nomeUrna;
 	}
+
 	public void setNomeUrna(String nomeUrna) {
 		this.nomeUrna = nomeUrna;
 	}
+
 	public int getVotosTotal() {
 		return votosTotal;
 	}
+
 	public void setVotosTotal(int votosTotal) {
 		this.votosTotal = votosTotal;
 	}
+
 	public int getNumero() {
 		return numero;
 	}
+
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
+
 	public String getDestVoto() {
 		return destVoto;
 	}
+	
 	public void setDestVoto(String destVoto) {
 		this.destVoto = destVoto;
 	}
