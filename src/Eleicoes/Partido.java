d17ee71ef4199df6e6af50dbd72189da2f664d17
bea <beamaia.work@gmail.com>
2021-03-25 23:16:24 -0300

    get votos totais

--- src/Eleicoes/Partido.java
+++ src/Eleicoes/Partido.java
@@ -9,58 +9,76 @@ public class Partido {
 	private int votosLegenda;
 	private int votosTotal;
 	private int numPartido;
-	private Set<Candidato> candidatos; 
-	
-	
+	private Set<Candidato> candidatos;
+
 	public Partido(String nome, String sigla, int vtL, int numP) {
 		this.nome = nome;
 		this.sigla = sigla;
 		this.votosLegenda = vtL;
-		this.votosTotal = -1; // TODO criar função que calcula os votos Totais 
+		this.votosTotal = -1; // TODO criar função que calcula os votos Totais
 		this.numPartido = numP;
-		
+
 		this.candidatos = new HashSet<Candidato>();
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getSigla() {
 		return sigla;
 	}
+
 	public void setSigla(String sigla) {
 		this.sigla = sigla;
 	}
+
 	public int getVotosLegenda() {
 		return votosLegenda;
 	}
+
 	public void setVotosLegenda(int votosLegenda) {
 		this.votosLegenda = votosLegenda;
 	}
+
 	public int getVotosTotal() {
 		return votosTotal;
 	}
+
 	public void setVotosTotal(int votosTotal) {
 		this.votosTotal = votosTotal;
 	}
+
 	public int getNumPartido() {
 		return numPartido;
 	}
+
 	public void setNumPartido(int numPartido) {
 		this.numPartido = numPartido;
 	}
-	
 	// Esses dois eu mudaria pra poder escolher qual candidato quer
+
 	// e para poder setar adicionando
 	public Set<Candidato> getCandidatos() {
 		return candidatos;
 	}
+
 	public void setCandidatos(Set<Candidato> candidatos) {
 		this.candidatos = candidatos;
 	}
-	
-	
+
+	public void getVotosTotais() {
+		int votosCandidatos = 0;
+
+		for (Candidato candidato : this.candidatos) {
+			votosCandidatos += candidato.getVotosTotal();
+		}
+
+		this.votosTotal = votosCandidatos + votosLegenda;
+	}
+
 }
