2d44eb57307a6b986171e608a2a4d4bb331b9cd8
bea <beamaia.work@gmail.com>
2021-03-28 14:39:48 -0300

    adidciona eleitosBeneficiados e função estatica

--- src/Eleicoes/Relatorio.java
+++ src/Eleicoes/Relatorio.java
@@ -1,5 +1,7 @@
 package Eleicoes;
 
+import java.text.DecimalFormat;
+
 public class Relatorio {
     private Eleicao eleicao;
 
@@ -7,6 +9,18 @@ public class Relatorio {
         this.eleicao = eleicao;
     }
     
+    static private String porcentagem (int valor, int total) {
+        double porcentagem = (double) valor * 100 / total;
+
+        DecimalFormat formatoDecimal = new DecimalFormat("#.00");
+        String porcentagemStr = formatoDecimal.format(porcentagem);     
+        
+        porcentagemStr = porcentagemStr.replace('.', ',');
+        porcentagemStr = porcentagemStr + "%";
+
+        return porcentagemStr;
+    }
+
     public void numeroDeVagas () {
         System.out.println("Número de vagas: " + eleicao.getVagas());
         System.out.println();
@@ -63,7 +77,19 @@ public class Relatorio {
     }
 
     public void eleitosBeneficiados () {
-        int contador;
+        int contador = 16;
+
+        System.out.println("Eleitos, que se beneficiaram do sistema proporcional:");
+        System.out.println("(com sua posição no ranking de mais votados)");
+
+        for (Candidato c: eleicao.getCandidatos().subList(15, eleicao.getCandidatos().size())) {
+            if (c.ehEleito())
+                System.out.println(contador + " - " + c);
+            
+            contador++;
+        }
+
+        System.out.println();
     }
 
     public void infoPartidos() {
@@ -100,14 +126,34 @@ public class Relatorio {
         System.out.println();
     }
 
+    public void eleitosPorIdade () {
+        int qtdMenos30 = this.eleicao.getCandidatosPorIdade(0, 30, eleicao.getDataEleicao());
+        int qtdMenos40 = this.eleicao.getCandidatosPorIdade(30, 40, eleicao.getDataEleicao()); 
+        int qtdMenos50 = this.eleicao.getCandidatosPorIdade(40, 50, eleicao.getDataEleicao());
+        int qtdMenos60 = this.eleicao.getCandidatosPorIdade(50, 60, eleicao.getDataEleicao());
+        int qtdMaiorIgual60 = this.eleicao.getCandidatosPorIdade(60, 1000, eleicao.getDataEleicao());
+        int qtdCandidatos = this.eleicao.getVagas();
+
+        System.out.println("Eleitos, por faixa etária (na data da eleição):");
+        System.out.printf("Idade < 30      : %d (%s)\n", qtdMenos30, porcentagem(qtdMenos30, qtdCandidatos));
+        System.out.printf("30 <= Idade < 40: %d (%s)\n", qtdMenos40, porcentagem(qtdMenos40, qtdCandidatos));
+        System.out.printf("40 <= Idade < 50: %d (%s)\n", qtdMenos50, porcentagem(qtdMenos50, qtdCandidatos));
+        System.out.printf("50 <= Idade < 60: %d (%s)\n", qtdMenos60, porcentagem(qtdMenos60, qtdCandidatos));
+        System.out.printf("60 <= Idade     : %d (%s)\n", qtdMaiorIgual60, porcentagem(qtdMaiorIgual60, qtdCandidatos));
+
+        System.out.println();
+    }
+
     public void eleitosPorGenero() {
     	int vagas = eleicao.getVagas();
     	int F = eleicao.calculaQtdF();
     	int M = vagas - F;
     	
     	System.out.println("Eleitos, por sexo:");
-    	System.out.printf("Feminino: %d (%.2f%%)\n", F, ((float)F*100/vagas));
-    	System.out.printf("Masculino: %d (%.2f%%)\n\n", M, ((float)M*100/vagas));
+    	System.out.printf("Feminino:  %d (%s)\n", F, porcentagem(F, vagas));
+    	System.out.printf("Masculino: %d (%s)\n", M, porcentagem(M, vagas));
+
+        System.out.println();
     }
     
     public void votosEleicao() {
@@ -117,9 +163,10 @@ public class Relatorio {
     	nominal = eleicao.getVotosNominais();
     	legenda = eleicao.getVotosLegenda();
     	
-    	
     	System.out.println("Total de votos válidos:    " + total);
-    	System.out.printf("Total de votos nominais:    %d (%.2f%%)\n",  nominal, ((float) nominal*100/total));
-    	System.out.printf("Total de votos nominais:    %d (%.2f%%)\n",  legenda, ((float) legenda*100/total));
+    	System.out.printf("Total de votos nominais:   %d (%s)\n",  nominal, porcentagem(nominal, total));
+    	System.out.printf("Total de votos de Legenda: %d (%s)\n",  legenda, porcentagem(legenda, total));
+        
+        System.out.println();
     }
 }
