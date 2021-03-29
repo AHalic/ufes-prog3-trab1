4f3bf2be50bac8320fa076fd0a65697bcd2bcbe0
AHalic <sophie.gama@aluno.ufes.br>
2021-03-28 21:07:35 -0300

    Adiciona definições de funções Pessoa

--- src/Eleicoes/Pessoa.java
+++ src/Eleicoes/Pessoa.java
@@ -8,24 +8,46 @@ public class Pessoa {
 	private String genero;
 	private LocalDate nascimento;
 	
+	/**
+	 * Constructor da classe Pessoa
+	 * @param nome
+	 * @param genero pode ser F ou M
+	 * @param nascimento 
+	 */
 	public Pessoa(String nome, String genero, LocalDate nascimento) {
 		this.nome = nome;
 		this.genero = genero;
 		this.nascimento = nascimento;
 	}
 	
+	/**
+	 * Retorna o nome desta Pessoa
+	 * @return nome
+	 */
 	public String getNome() {
 		return nome;
 	}
 
+	/**
+	 * Retorna o genero desta Pessoa
+	 * @return genero
+	 */
 	public String getGenero() {
 		return genero;
 	}
 	
+	/**
+	 * Retorna o nascimento desta Pessoa
+	 * @return nascimento
+	 */
 	public LocalDate getNascimento() {
 		return nascimento;
 	}
 	
+	/**
+	 * Verifica se o genero desta Pessoa é F 
+	 * @return true se sim, false se não
+	 */
 	public boolean ehGeneroF () {
 		if (this.genero.equals("F"))
 			return true;
@@ -33,10 +55,20 @@ public class Pessoa {
 			return false;
 	}
 	
+	/**
+	 * Retorna uma string de representação desta Pessoa
+	 * @return nome nome desta Pessoa em maiúsculo
+	 */
+	@Override
 	public String toString () {
 		return this.nome.toUpperCase();
 	}
 
+	/**
+	 * Retorna a idade desta Pessoa com base em uma data
+	 * @param dia data para cálculo da idade
+	 * @return idade 
+	 */
 	public int getIdade (LocalDate dia) {
 		return Period.between(this.nascimento, dia).getYears();
 	}
